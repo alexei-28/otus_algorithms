c98428ed1632bac04430956505e46f542a082e1a
alexei.zhelev <alexei.jelev@qsystems.md>
2021-01-01 13:51:17 +0200

    Squashed commit of the following:
    commit 43ac767b75550c0fb1f8c67cd17dec89050bde57
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Fri Jan 1 13:49:39 2021 +0200
    
        not finish
    
    commit 3cde2d56a912cfbbe1a925f3db83f3e2057399fc
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Thu Dec 31 15:45:52 2020 +0200
    
        c
    
    commit c89467972d996efe9042f9498d0f2da9226d6ae4
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Thu Dec 31 15:45:25 2020 +0200
    
        c
    
    commit 0286f8a6f8ac0e4d8c34cc991beb22d0add3269b
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Thu Dec 31 15:43:04 2020 +0200
    
        c
    
    commit 37fcc9ba9e039ce3e0e25efb90964e6b973b48f8
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Thu Dec 31 15:35:39 2020 +0200
    
        c
    
    commit a270b242bc7856090ff77292574837aa9b7b61c4
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Thu Dec 31 12:10:30 2020 +0200
    
        c
    
    commit db8e369bb5cd1914f4d04ecfe6798a86951da644
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Thu Dec 31 12:08:26 2020 +0200
    
        c
    
    commit e04b5d7342c8d8199e18e1d2664381f8db19ad58
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Thu Dec 31 12:03:52 2020 +0200
    
        c
    
    commit 8c6ab49c85c1b7d391b86f967a258a7ba8eea643
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Thu Dec 31 11:45:46 2020 +0200
    
        c
    
    commit 76468f42328a8bdcd0f121e4fa0547791dd46b24
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Thu Dec 31 11:43:20 2020 +0200
    
        init
    
    commit 5a6e1b58bc0a13f681c1426c7240f46f24cefbee
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Thu Dec 31 11:42:51 2020 +0200
    
        init
    
    commit 45f2f50628fcaaa7f45752097a7685f0708b1909
    Author: alexei.zhelev <alexei.jelev@qsystems.md>
    Date:   Sat Dec 26 21:15:09 2020 +0200
    
        homework#10

---
+++ src/main/java/ru/otus/algorithms/homework10/Main.java
@@ -0,0 +1,12 @@
+package ru.otus.algorithms.homework10;
+
+/*-
+    Entry point for homework#10
+
+*/
+public class Main {
+    public static void  main(String[] args) {
+        System.out.println("Homework #10!\nJDK: " + System.getProperty("java.version"));
+        new AlgorithmKosaraju();
+    }
+}
