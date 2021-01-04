b6e55614a50bad0bc5fed560b6ad5bcfc378bf7f
alexei.zhelev <a_subscriber@mail.ru>
2021-01-04 16:19:57 +0200

    w

--- src/main/java/ru/otus/algorithms/homework10/DFS.java
+++ src/main/java/ru/otus/algorithms/homework10/DFS.java
@@ -31,8 +31,11 @@ package ru.otus.algorithms.homework10;
         вернуть ложь
 */
 public class DFS {
+    private int[] array = new int[] {0,1,2,3,4,5,6,7,8,9};
+    private int[] visitedArray = new int[array.length];
 
     public void dfsByRecursive() {
+        System.out.println("dfsByRecursive");
 
     }
 
--- src/main/java/ru/otus/algorithms/homework10/Main.java
+++ src/main/java/ru/otus/algorithms/homework10/Main.java
@@ -7,6 +7,6 @@ package ru.otus.algorithms.homework10;
 public class Main {
     public static void  main(String[] args) {
         System.out.println("Homework #10!\nJDK: " + System.getProperty("java.version"));
-        new AlgorithmKosaraju();
+        new DFS();
     }
 }
