df66920135e9abf10dc5d5c5e91419c8693dc326
alexei.zhelev <a_subscriber@mail.ru>
2021-01-04 16:36:02 +0200

    w

--- src/main/java/ru/otus/algorithms/homework10/DFS.java
+++ src/main/java/ru/otus/algorithms/homework10/DFS.java
@@ -32,7 +32,7 @@ package ru.otus.algorithms.homework10;
 */
 public class DFS {
     private int[] array = new int[] {0,1,2,3,4,5,6,7,8,9};
-    private int[] visitedArray = new int[array.length];
+    private boolean[] used = new boolean[array.length];
 
     public void dfsByRecursive() {
         System.out.println("dfsByRecursive");
